53c6a35cd2a7e6053a7aff49d2bfd8c6aad567fd
Arkar Min <arkarmm2003@gmail.com>
2024-12-11 00:11:27 -0500

    font changes

--- index.php
+++ index.php
@@ -1,27 +1,66 @@
 <?php $page_title = 'Home';
- include 'includes/header.html'; 
- ?>
+include 'includes/header.html';
+?>
 
-<div class="containerA">
-    <div class="announcement">
-        <section class="announce">
-            <h1>Announcements</h1>
-        </section>
+<div class="content">
+    <div class="containerA">
+        <div class="announcement">
+            <section class="announce">
+                <h1>Announcements</h1>
+            </section>
 
-        <section class="info">
-            <p>Show Date & Musicians</p>
-        </section>
+            <section class="info">
+                <p>Show Date & Musicians</p>
+            </section>
 
-        <section class="title">
-            <p>10:30pm Monday THE GIGGOLOS</p>
-        </section>
+            <section class="title">
+                <table>
+                    <tr>
+                        <th>Date</th>
+                        <th>Time</th>
+                        <th>Musicians</th>
+                    </tr>
+
+                    <tr>
+                        <td>12.12.24</td>
+                        <td>12:40pm</td>
+                        <td>Giggolos</td>
+                    </tr>
+                    <tr>
+                        <td>12.12.24</td>
+                        <td>12:40pm</td>
+                        <td>Giggolos</td>
+                    </tr>
+                    <tr>
+                        <td>12.12.24</td>
+                        <td>12:40pm</td>
+                        <td>Giggolos</td>
+                    </tr>
+                    <tr>
+                        <td>12.12.24</td>
+                        <td>12:40pm</td>
+                        <td>Giggolos</td>
+                    </tr>
+                    <tr>
+                        <td>12.12.24</td>
+                        <td>12:40pm</td>
+                        <td>Giggolos</td>
+                    </tr>
+                    <tr>
+                        <td>12.12.24</td>
+                        <td>12:40pm</td>
+                        <td>Giggolos</td>
+                    </tr>
+                </table>
+            </section>
+        </div>
     </div>
-</div>
-<audio id="player" autoplay loop>
-    <source src="Moanin.mp3"  type="audio/mp3">
+    <audio id="player" autoplay loop>
+    <source src="deepinit.mp3"  type="audio/mp3">
 </audio>
+</div>
 <script>
-    let audio=document.getElementById("player");
-    audio.volume =0.2;
+    let audio = document.getElementById("player");
+    audio.volume = 0.2;
 </script>
 <?php include 'includes/footer.html'; ?>
